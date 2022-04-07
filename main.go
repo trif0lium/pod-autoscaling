1b678c56fb5bfb73605ac40a00d47589f9d8b938
trif0lium <gh@fleeingsunlight.dev>
2022-04-07 15:44:04 +0700

    feat(go): init main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo-contrib/prometheus"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", fib)
+
+	p := prometheus.NewPrometheus("echo", nil)
+	p.Use(e)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func fib(c echo.Context) error {
+	min := 40
+	max := 46
+	rand.Seed(time.Now().UnixNano())
+	return c.String(http.StatusOK, fmt.Sprint(_fib(rand.Intn(max-min)+min)))
+}
+
+func _fib(n int) uint64 {
+	if n <= 1 {
+		return 1
+	}
+	return _fib(n-1) + _fib(n-2)
+}
