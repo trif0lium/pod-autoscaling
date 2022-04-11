f625348e74654dc32e0edec8c4548d20f0f51e32
trif0lium <gh@fleeingsunlight.dev>
2022-04-11 20:20:27 +0700

    feat: init k6.js

---
+++ k6.js
@@ -0,0 +1,27 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export const options = {
+  stages: [
+    // 00:00 - 08:00
+    { duration: '2m', target: getRandomIntInclusive(30, 45) },
+    { duration: '2m', target: getRandomIntInclusive(5, 15) },
+    { duration: '2m', target: getRandomIntInclusive(0, 5) },
+    { duration: '2m', target: getRandomIntInclusive(5, 15) },
+    // 08:00 - 12:00
+    { duration: '2m', target: getRandomIntInclusive(75, 100) },
+    { duration: '2m', target: getRandomIntInclusive(75, 100) },
+    // 12:00 - 16.00
+    { duration: '2m', target: getRandomIntInclusive(85, 100) },
+    { duration: '2m', target: getRandomIntInclusive(85, 100) },
+    // 16:00 - 22.00
+    { duration: '2m', target: getRandomIntInclusive(50, 75) },
+    { duration: '2m', target: getRandomIntInclusive(50, 75) },
+    { duration: '2m', target: getRandomIntInclusive(45, 60) },
+    // 22:00 - 00.00
+    { duration: '2m', target: getRandomIntInclusive(30, 45) },
+  ]
+}
