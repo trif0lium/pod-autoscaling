d7dbb761892d135f91edb4d2c1ebf403689eaaf4
trif0lium <gh@fleeingsunlight.dev>
2022-04-13 13:41:17 +0700

    chore(k6): targetNumberOfVUs

--- k6.js
+++ k6.js
@@ -1,31 +1,31 @@
 import http from 'k6/http'
 import { sleep } from 'k6'
 
-function getRandomIntInclusive(min, max) {
+function targetNumberOfVUs(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.ceil(Math.floor(Math.random() * (max - min + 1) + min)) * 5
 }
 
 export const options = {
   stages: [
     // 00:00 - 08:00
-    { duration: '2m', target: getRandomIntInclusive(300, 450) },
-    { duration: '2m', target: getRandomIntInclusive(50, 150) },
-    { duration: '2m', target: getRandomIntInclusive(0, 50) },
-    { duration: '2m', target: getRandomIntInclusive(50, 150) },
+    { duration: '2m', target: targetNumberOfVUs(300, 450) },
+    { duration: '2m', target: targetNumberOfVUs(50, 150) },
+    { duration: '2m', target: targetNumberOfVUs(0, 50) },
+    { duration: '2m', target: targetNumberOfVUs(50, 150) },
     // 08:00 - 12:00
-    { duration: '2m', target: getRandomIntInclusive(750, 1000) },
-    { duration: '2m', target: getRandomIntInclusive(750, 1000) },
+    { duration: '2m', target: targetNumberOfVUs(750, 1000) },
+    { duration: '2m', target: targetNumberOfVUs(750, 1000) },
     // 12:00 - 16.00
-    { duration: '2m', target: getRandomIntInclusive(850, 1000) },
-    { duration: '2m', target: getRandomIntInclusive(850, 1000) },
+    { duration: '2m', target: targetNumberOfVUs(850, 1000) },
+    { duration: '2m', target: targetNumberOfVUs(850, 1000) },
     // 16:00 - 22.00
-    { duration: '2m', target: getRandomIntInclusive(500, 750) },
-    { duration: '2m', target: getRandomIntInclusive(500, 750) },
-    { duration: '2m', target: getRandomIntInclusive(450, 600) },
+    { duration: '2m', target: targetNumberOfVUs(500, 750) },
+    { duration: '2m', target: targetNumberOfVUs(500, 750) },
+    { duration: '2m', target: targetNumberOfVUs(450, 600) },
     // 22:00 - 00.00
-    { duration: '2m', target: getRandomIntInclusive(300, 450) },
+    { duration: '2m', target: targetNumberOfVUs(300, 450) },
   ]
 }
 
