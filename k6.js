00889c1925d7a674bfaafe2e8d54f29f4bd6c87e
trif0lium <gh@fleeingsunlight.dev>
2022-04-13 15:21:26 +0700

    chore(k6): 20 -> 5

--- k6.js
+++ k6.js
@@ -4,7 +4,7 @@ import { sleep } from 'k6'
 function targetNumberOfVUs(min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.ceil(Math.floor(Math.random() * (max - min + 1) + min)) * 20
+  return Math.ceil(Math.floor(Math.random() * (max - min + 1) + min)) * 5
 }
 
 export const options = {
