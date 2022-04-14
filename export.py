2869016cf8b216af267f6e56e767193c978fed00
trif0lium <gh@fleeingsunlight.dev>
2022-04-14 16:28:01 +0700

    feat(export): write_csv_file()

--- export.py
+++ export.py
@@ -1,9 +1,30 @@
 import csv
 import requests
+import sys
 
 PROMETHEUS = 'http://localhost:9090'
 
 def get_metrics():
-    response = requests.get('{0}/api/label/__name__/values'.format(PROMETHEUS))
+    response = requests.get('{0}/api/v1/label/__name__/values'.format(PROMETHEUS))
     metrics = response.json()['data']
     return metrics
+
+def write_csv_file(metric_name):
+    response = requests.get('{0}/api/v1/query'.format(PROMETHEUS), params = {'query': metric_name + '[1h]' })
+    results = response.json()['data']['result']
+
+    labels = set()
+    for result in results:
+        label.update(result['metric'].keys())
+
+    labels.discard('__name__')
+    labels = sorted(labels)
+
+    writer = csv.writer(sys.stdout)
+    writer.writerow(['name', 'timestamp', 'value'] + labels)
+
+    for result in results:
+        l = [result['metric'].get('__name__', '')] + result['value']
+        for label in labels:
+            l.append(result['metric'].get(label, ''))
+        writer.writerow(l)
